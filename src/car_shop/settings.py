721fe9c880a1177ff58d846460d00dac4cead800
yegorisB <yegor.b.isakov@gmail.com>
2023-06-05 11:15:05 -0400

    guest user removed, secret_key removed

--- src/car_shop/settings.py
+++ src/car_shop/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 #SECRET_KEY = config['SECRET_KEY']
-SECRET_KEY = "django-insecure-s#2cpy%w0t@xltso&2^=j-s+ywk)l@l_)ahnv-7-xcu4*%j7&0"
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
